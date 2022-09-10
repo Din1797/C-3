4ed184f1dc29ded342e8ec14115197aab739be4a
Din <hurshid.17.97@mail.ru>
2022-09-11 03:03:10 +0500

    teg1

---
+++ teg1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 10000;
+int n2 = n / 1000 % 10;
+int n4 = n / 10 % 10;
+int n5 = n % 10;
+Console.WriteLine($" n1= {n1} n2= {n2} n4= {n4} n5= {n5} ");
+ if (n1 == n5 || n2 == n4)
+{
+ Console.WriteLine($"Ваше число: {n} - палиндром.");
+}
+else
+{
+    Console.WriteLine($"Ваше число: {n} - НЕ палиндром.");
+}
