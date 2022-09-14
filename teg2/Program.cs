f2f26ef07deadac8a35e5b9be6fac1d84ca84998
Din1797 <112232654+Din1797@users.noreply.github.com>
2022-09-14 14:36:49 +0500

    Update Program.cs

--- teg2/Program.cs
+++ teg2/Program.cs
@@ -12,5 +12,5 @@ int x2 = Convert.ToInt32(Console.ReadLine());
 int y2 = Convert.ToInt32(Console.ReadLine());
 int z2 = Convert.ToInt32(Console.ReadLine());
 
-double AB = Math.Sprt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+double AB = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
 Console.WriteLine(AB);
